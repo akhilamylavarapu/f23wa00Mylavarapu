6a9f340c5d5677c000243cb09de00d3687c977d0
AkhilaMylavarapu <akhila.smr23@gmail.com>
2023-10-24 17:11:01 -0500

    modified users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+var express = require('express');
+var userRouter = express.Router();
+var sum = 0;
+var more = 0;
+
+userRouter.get('/', function(req,res) {
+  more++;
+  sum += more;
+  res.send(`Sum is: ${sum}`);
 });
 
-module.exports = router;
+//module.exports = router;
+module.exports = userRouter;

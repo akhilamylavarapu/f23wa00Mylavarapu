233bae43618374798f691b15cf2bcfa083c8d7af
AkhilaMylavarapu <akhila.smr23@gmail.com>
2023-10-25 10:20:23 -0500

    added newend point computation in users.js

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use('/mydata', mydataRouter);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/computation', mydataRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -17,5 +17,13 @@ userRouter.get('/', function(req,res) {
   res.send(`Sum is: ${sum}`);
 });
 
+userRouter.get('/computation', function(req, res, next){
+  var x = req.x;
+  if(!x){
+    x = Math.random()*1000;
+  }
+  res.send(`Math.round(${x}) is: ${Math.round(x)}`);
+});
+
 //module.exports = router;
 module.exports = userRouter;
